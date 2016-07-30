1efb7c41a80b60c1edef7acd8339e1f62ba563a4
Sam Watson <sam@toomuch.com.au>
2016-07-30 19:58:52 +1000

    Added bitfinex package and updated it for Python 3

---
+++ bitfinex/pyfinex.py
@@ -0,0 +1,311 @@
+import websocket
+import threading
+import time
+from time import sleep
+import json
+import hmac
+import hashlib
+
+# API JSON KEYS
+
+KEY_CHANNEL_NAME = "channel"
+KEY_CHANNEL_ID = "chanId"
+KEY_EVENT = "event"
+
+# COMMON API JSON VALUES
+
+EVENT_SUBSCRIBED = "subscribed"
+
+# USEFUL STRINGS
+
+BFX_WEBSOCKET_ADDRESS = "wss://api2.bitfinex.com:3000/ws"
+
+BOOK_SUBSCRIBE_STRING = "{\"event\":\"subscribe\",\"channel\":\"book\",\"pair\":\"BTCUSD\",\"prec\":\"P0\"}"
+TICKER_SUBSCRIBE_STRING = "{\"event\":\"subscribe\",\"channel\":\"ticker\",\"pair\":\"BTCUSD\"}"
+TRADES_SUBSCRIBE_STRING = "{\"event\":\"subscribe\",\"channel\":\"trades\",\"pair\":\"BTCUSD\"}"
+
+class BitfinexWebsocket:
+    
+    def __init__(self, debug = False):
+        self.debug = debug
+        if (self.debug):
+            websocket.enableTrace(True)
+            
+        self.ws = websocket.WebSocketApp(BFX_WEBSOCKET_ADDRESS,
+        on_message = self.__on_message,
+        on_error = self.__on_error,
+        on_close = self.__on_close)
+        self.ws.on_open = self.__on_open
+        self.wst = threading.Thread(target = self.ws.run_forever)
+        self.wst.daemon = True
+        self.wst.start()
+        
+        # block main thread for at most 5 seconds or until websocket is connected
+        connection_timeout = 5
+        while not self.ws.sock.connected and connection_timeout:
+                sleep(1)
+                connection_timeout -= 1
+        
+    def subscribe_book(self, callback):
+        print("subscribing to BTCUSD book")
+        self.ws.send(BOOK_SUBSCRIBE_STRING);
+        self.book_callback = callback
+
+    def subscribe_ticker(self, callback):
+        print("subscribing to BTCUSD ticker")
+        self.ws.send(TICKER_SUBSCRIBE_STRING);
+        self.ticker_callback = callback
+        
+    def subscribe_trades(self, callback):
+        print("subscribing to BTCUSD trades")
+        self.ws.send(TRADES_SUBSCRIBE_STRING);
+        self.trades_callback = callback
+        
+    def subscribe_private(self, api_key, api_secret, callbacks):
+        if (type(callbacks) is dict):
+            if 'private_wallet_callback' in callbacks.keys():
+                self.private_wallet_callback = callbacks['private_wallet_callback']
+            if 'private_order_callback' in callbacks.keys():
+                self.private_order_callback = callbacks['private_order_callback']
+            if 'private_position_callback' in callbacks.keys():
+                self.private_position_callback = callbacks['private_position_callback']
+            if 'trade_callback' in callbacks.keys():
+                self.trade_callback = callbacks['trade_callback']
+        
+        ### JS authentication example from API docs ###
+#         var
+#             crypto = require('crypto'),
+#             api_key = 'API_KEY',
+#             api_secret = 'API_SECRET',
+#             payload = 'AUTH' + (new Date().getTime()),
+#             signature = crypto.createHmac("sha384", api_secret).update(payload).digest('hex');
+#         w.send(JSON.stringify({
+#             event: "auth",
+#             apiKey: api_key,
+#             authSig: signature,
+#             authPayload: payload
+#         }));
+
+        payload = "AUTH" + str(int(time.time() * 1000))
+        signature = hmac.new(api_secret, payload, hashlib.sha384).hexdigest()
+        auth_request = json.dumps({"event": "auth", "apiKey": api_key, "authSig": signature, "authPayload": payload})
+        
+        if (self.debug):
+            print("API Key: " + api_key)
+            print("Signature Payload: " + payload)
+            print("Signature: " + signature)
+            print(auth_request)
+            
+        self.ws.send(auth_request)
+        
+    def __update_book(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "book_callback")):
+            price = update_object[0]
+            count = update_object[1]
+            amount = update_object[2]
+            self.book_callback(price, count, amount, False)
+        
+    def __parse_book_message(self, message_object):
+        if (len(message_object) > 1):
+            if (type(message_object[1]) is list):
+                book_updates = message_object[1]
+                
+                if (hasattr(self, "book_callback")):
+                    self.book_callback(None, None, None, True)
+                
+                for update_object in book_updates:
+                    self.__update_book(update_object)
+            else:
+                message_object.pop(0)
+                self.__update_book(message_object)
+    
+    def __update_ticker(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "ticker_callback")):
+            bid = update_object[0]
+            bid_size = update_object[1]
+            ask = update_object[2]
+            ask_size = update_object[3]
+            daily_change = update_object[4]
+            daily_change_percentage = update_object[5]
+            last_price = update_object[6]
+            volume = update_object[7]
+            high = update_object[8]
+            low = update_object[9]
+            self.ticker_callback(bid, bid_size, ask, ask_size, daily_change, daily_change_percentage, last_price, volume, high, low)
+        
+    def __parse_ticker_message(self, message_object):
+        message_object.pop(0)
+        if (len(message_object) == 10):
+            self.high = message_object[8]
+            self.low = message_object[9]
+            self.__update_ticker(message_object)
+        elif (len(message_object) == 8):
+            last_price = message_object[6]
+            if (hasattr(self, "high")):
+                if (last_price > self.high):
+                    self.high = last_price
+            else:
+                self.high = last_price
+                
+            if (hasattr(self, "low")):
+                if (last_price < self.low):
+                    self.low = last_price
+            else:
+                self.low = last_price
+                
+            message_object.append(self.high)
+            message_object.append(self.low)
+            self.__update_ticker(message_object)
+    
+    def __update_trades(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "trades_callback")):
+            sequence_id = update_object[0]
+            timestamp = update_object[1]
+            price = update_object[2]
+            amount = update_object[3]
+            self.trades_callback(sequence_id, timestamp, price, amount)
+        
+    def __parse_trades_message(self, message_object):
+        if (len(message_object) > 1):
+            if (type(message_object[1]) is list):
+                trade_updates = message_object[1]
+                for update_object in trade_updates:
+                    self.__update_trades(update_object)
+            else:
+                message_object.pop(0)
+                self.__update_trades(message_object)
+                
+    def __update_private_wallet(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "private_wallet_callback")):
+            name = update_object[0]
+            currency = update_object[1]
+            balance = update_object[2]
+            unsettled_interest = update_object[3]
+            self.private_wallet_callback(name, currency, balance, unsettled_interest)
+            
+    def __update_private_position(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "private_position_callback")):
+            pair = update_object[0]
+            status = update_object[1]
+            amount = update_object[2]
+            base_price = update_object[3]
+            margin_funding = update_object[4]
+            margin_funding_type = update_object[5]
+            self.private_position_callback(pair, status, amount, base_price, margin_funding, margin_funding_type)
+            
+    def __update_private_order(self, update_object):
+        if (self.debug):
+            print(update_object)
+            
+        if (hasattr(self, "private_order_callback")):
+            order_id = update_object[0]
+            pair = update_object[1]
+            amount = update_object[2]
+            original_amount = update_object[3]
+            order_type = update_object[4]
+            status = update_object[5]
+            price = update_object[6]
+            price_average = update_object[7]
+            created_at = update_object[8]
+            notify = update_object[9]
+            hidden = update_object[10]
+            self.private_order_callback(order_id, pair, amount, original_amount, order_type, status, price, price_average, created_at, notify, hidden)
+            
+    def __update_private_trade(self, update_object):
+        if (self.debug):
+            print(update_object)
+                
+    def __parse_private_message(self, message_object):
+        print(message_object)
+        message_object.pop(0)
+        message_type = message_object.pop(0)
+        
+        if (message_type == "ws"):
+            for update_object in message_object[0]:
+                self.__update_private_wallet(update_object)
+        elif (message_type == "wu"):
+            update_object = message_object[0]
+            self.__update_private_wallet(update_object)
+        elif (message_type == "ps"):
+            for update_object in message_object[0]:
+                self.__update_private_position(update_object)
+        elif (message_type == "pn" or message_type == "pu" or message_type == "pc"):
+            update_object = message_object[0]
+            self.__update_private_position(update_object)
+        elif (message_type == "os"):
+            for update_object in message_object[0]:
+                self.__update_private_order(update_object)
+        elif (message_type == "on" or message_type == "ou" or message_type == "oc"):
+            update_object = message_object[0]
+            self.__update_private_order(update_object)
+        elif (message_type == "ts"):
+            pass
+        elif (message_type == "te"):
+            pass
+        elif (message_type == "tu"):
+            pass
+
+    def __on_message(self, ws, message):
+        obj = json.loads(message);
+        
+        if (type(obj) is dict):
+            if KEY_EVENT in obj.keys():
+                if (obj[KEY_EVENT] == EVENT_SUBSCRIBED):
+                    channel = obj[KEY_CHANNEL_NAME]
+                    if (channel == "book"):
+                        self.book_channel_id = obj[KEY_CHANNEL_ID];
+                        print("subscribed to the orderbook")
+                    elif (channel == "ticker"):
+                        self.ticker_channel_id = obj[KEY_CHANNEL_ID];
+                        print("subscribed to the ticker")
+                    elif (channel == "trades"):
+                        self.trades_channel_id = obj[KEY_CHANNEL_ID];
+                        print("subscribed to the trades")
+                elif (obj[KEY_EVENT] == "auth"):
+                    status = obj["status"]
+                    if (status == "OK"):
+                        # should always be channel id 0
+                        self.private_channel_id = obj[KEY_CHANNEL_ID]
+                    elif (status == "FAIL"):
+                        # should throw exception here
+                        print("Error: " + obj["code"])
+                        
+        elif (type(obj) is list):
+            if (len(obj) > 0):
+                channel_id = obj[0]
+                if (hasattr(self, "book_channel_id")):
+                    if (channel_id == self.book_channel_id):
+                        self.__parse_book_message(obj)
+                if (hasattr(self, "ticker_channel_id")):
+                    if (channel_id == self.ticker_channel_id):
+                        self.__parse_ticker_message(obj)
+                if (hasattr(self, "trades_channel_id")):
+                    if (channel_id == self.trades_channel_id):
+                        self.__parse_trades_message(obj)
+                if (hasattr(self, "private_channel_id")):
+                    if (channel_id == self.private_channel_id):
+                        self.__parse_private_message(obj)
+
+    def __on_error(self, ws, error):
+        print(error)
+
+    def __on_close(self, ws):
+        print("### closed connection to " + BFX_WEBSOCKET_ADDRESS + " ###")
+
+    def __on_open(self, ws):
+        print("### opened connection to " + BFX_WEBSOCKET_ADDRESS + " ###")
---
+++ bitfinex/pyfinex_test.py
@@ -0,0 +1,53 @@
+from time import sleep
+import time
+
+from pyfinex import PyfinexWebsocket
+
+def ticker_update(bid, bid_size, ask, ask_size, daily_change, daily_change_percentage, last_price, volume, high, low):
+    price_range = high - low
+    price_step = price_range / 50
+    last_price_string = "{:.2f}".format(last_price)
+    low_price_string = "{:.2f}".format(low)
+    high_price_string = "{:.2f}".format(high)
+    
+    price_string = str(int(time.time() * 1000)) + ": " + low_price_string + "-"
+    current = low
+    dash_count = 0
+    while current < last_price:
+        current += price_step
+        dash_count += 1
+        price_string += "-"
+        
+    price_string += last_price_string
+    for i in range(dash_count, 50):
+        price_string += "-"
+    price_string += "-" + high_price_string
+        
+    print price_string
+    
+def trades_update(sequence_id, timestamp, price, amount):
+    buy_sold = "sold" if (amount < 0) else "bought"
+    trade_string = "trade " + str(sequence_id) + " at " + str(timestamp) + ": " + str(abs(amount)) + " " + buy_sold + " at " + str(price)
+    print trade_string
+    
+def book_update(price, count, amount, clear):
+    if not clear:
+        bid_ask = "bid" if (amount > 0) else "ask"
+        book_string = str(price) + " " + bid_ask + " for " + str(abs(amount)) + " BTC"
+        print book_string
+    else:
+        print "Orderbook cleared!"
+
+if __name__ == "__main__":
+    pf = PyfinexWebsocket()
+    pf.subscribe_ticker(ticker_update)
+    # pf.subscribe_trades(trades_update)
+    # pf.subscribe_book(book_update)
+    
+    # lines = [line.strip() for line in open('api.keys')]
+    # api_key = lines[0]
+    # api_secret = lines[1]
+    # pf.subscribe_private(api_key, api_secret, 'callbacks')
+    
+    while True:
+        sleep(1)
