5b33aa3f060f434171d55e017f3f0a1fe41878b9
Sam Watson <sam@toomuch.com.au>
2016-08-13 17:40:41 +1000

    refactored request_depth into it's own method

--- kraken/kraken.py
+++ kraken/kraken.py
@@ -3,7 +3,11 @@ import krakenex
 class Kraken:
     
     krakenex = None
+    update_interval = 10
     
     def __init__(self):
         self.krakenex = krakenex.API()
-        print(self.krakenex.query_public('Depth', {'pair': 'XXBTZUSD', 'count': '1'}))
+        print(self.request_depth('XXBTZUSD'))
+        
+    def request_depth(self, pair):
+        return self.krakenex.query_public('Depth', {'pair': pair, 'count': '1'})
