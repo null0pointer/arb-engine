d8d0f1317814b6a2c7f307aca5c35c1acc0c359a
Sam Watson <swatson@MEL01-UMAC018.local>
2016-08-15 14:25:57 +1000

    Made the kraken module loop the depth request

--- kraken/kraken.py
+++ kraken/kraken.py
@@ -7,7 +7,8 @@ class Kraken:
     
     def __init__(self):
         self.krakenex = krakenex.API()
-        print(self.request_depth('XXBTZUSD'))
+        while True:
+            print(self.request_depth('XXBTZUSD'))
         
     def request_depth(self, pair):
         return self.krakenex.query_public('Depth', {'pair': pair, 'count': '1'})
